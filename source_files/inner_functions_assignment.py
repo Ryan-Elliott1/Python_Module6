048da1acae916631a7f978a7f7d4d484fb33d384
Ryan-Elliott1 <60247044+Ryan-Elliott1@users.noreply.github.com>
2020-06-16 16:06:52 -0500

    inner function docstrings

--- source_files/inner_functions_assignment.py
+++ source_files/inner_functions_assignment.py
@@ -1,4 +1,18 @@
+"""
+Program: inner_functions_assignment.py
+Author: Ryan Elliott
+Last date modified: 06/16/2020
+This program uses a list and inner functions to calculate perimeter and area
+Input list of measurements
+Outputs the perimeter and area of measurements
+"""
+
+
 def measurements(measurement_list):
+    """Returns
+    :param measurement_list: list of measurements
+    :returns perimeter and area of measurements
+    """
     def area(a_list):
         if len(a_list) == 1:
             return a_list[0] * a_list[0]
