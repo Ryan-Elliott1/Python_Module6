37be805a22131447100a7ccc46ba1283f13680ad
Ryan-Elliott1 <60247044+Ryan-Elliott1@users.noreply.github.com>
2020-06-16 16:04:27 -0500

    inner_function test fix

--- source_files/inner_functions_assignment.py
+++ source_files/inner_functions_assignment.py
@@ -1,13 +1,19 @@
 def measurements(measurement_list):
     def area(a_list):
-        return a_list[0] * a_list[1]
+        if len(a_list) == 1:
+            return a_list[0] * a_list[0]
+        else:
+            return a_list[0] * a_list[1]
 
     def perimeter(a_list):
-        return (a_list[0] * 2) + (a_list[1] * 2)
+        if len(a_list) == 1:
+            return a_list[0] * 4
+        else:
+            return (a_list[0] * 2) + (a_list[1] * 2)
 
     output = "Perimeter = " + str(perimeter(measurement_list)) + " Area = " + str(area(measurement_list))
     return output
 
 
 if __name__ == '__main__':
-    print(measurements([2.1, 3.4]))
+    print(measurements([3.5]))
